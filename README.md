07db8bf87f4c5d714dfe5951f1af0b186aafb5c5
Bradley Sharkey <15859442+basharkey@users.noreply.github.com>
2022-02-12 16:23:43 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# AWS DDNS
+
+Dynamic DNS program for updating AWS DNS A records based on your public IP.
+
+
+Requires AWS CLI configured on host and user with following policy permissions:
+
+```
+route53:ChangeResourceRecordSets
+route53:ListResourceRecordSets
+```
+
+
+Usage:
+
+```
+aws-ddns.py -r ddns.example.com -z ABCDE12345ABCDE123456
+aws-ddns.py --record ddns.example.com --hosted-zone-id ABCDE12345ABCDE123456
+```
---
+++ aws-ddns.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import requests
+import boto3
+import argparse
+
+def get_aws_record_ip(hosted_zone_id, record_name):
+    aws_records = boto3.client('route53').list_resource_record_sets(HostedZoneId=hosted_zone_id)
+
+    for record in aws_records['ResourceRecordSets']:
+        if record['Name'] == record_name:
+            aws_record_ip = record['ResourceRecords'][0]['Value']
+            return(aws_record_ip)
+    raise SystemExit(f"Unable to find record \"{record_name}\" in hosted zone {hosted_zone_id}")
+
+parser = argparse.ArgumentParser(description="AWS DDNS record updater")
+parser.add_argument('-r', '--record',  metavar='record_name', required=True, help="AWS record name")
+parser.add_argument('-z', '--hosted-zone-id', metavar='hosted_zone_id', required=True, help="AWS hosted zone id")
+args = parser.parse_args()
+
+r = requests.get('https://checkip.amazonaws.com')
+if r.status_code != 200:
+    raise SystemExit("Unable to retrieve public IP")
+public_ip = r.text.strip('\n')
+
+change_batch = {
+    'Comment': f'{record_name} DDNS record update',
+    'Changes': [
+        {
+            'Action': 'UPSERT',
+            'ResourceRecordSet': {
+                'Name': record_name,
+                'Type': 'A',
+                'TTL': 300,
+                'ResourceRecords': [
+                    {
+                        'Value': public_ip
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+aws_record_ip = get_aws_record_ip(hosted_zone_id, record_name)
+if aws_record_ip != public_ip:
+    print(f"\"{record_name}\" record IP ({aws_record_ip}) and public IP ({public_ip}) do not match, updating...")
+    boto3.client('route53').change_resource_record_sets(HostedZoneId=hosted_zone_id, ChangeBatch=change_batch)
+
+    aws_record_ip = get_aws_record_ip(hosted_zone_id, record_name)
+    if aws_record_ip == public_ip:
+        print(f"\"{record_name}\" successfully updated to {aws_record_ip}")
+    else:
+        raise SystemExit("Record update failed")
