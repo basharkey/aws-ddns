6d26a7ae2f7302c7dce5c918933dc0e51808a430
basharkey <15859442+basharkey@users.noreply.github.com>
2022-12-25 20:11:56 -0500

    multiple record updates in one command

--- aws-ddns.py
+++ aws-ddns.py
@@ -14,11 +14,11 @@ def get_aws_record_ip(hosted_zone_id, record_name):
     raise SystemExit(f"Unable to find record {repr(record_name)} in hosted zone {hosted_zone_id}")
 
 parser = argparse.ArgumentParser(description="AWS DDNS record updater")
-parser.add_argument('-r', '--record',  metavar='record_name', required=True, help="AWS record name")
+parser.add_argument('-r', '--record',  metavar='record_name', action='append', required=True, help="AWS record name")
 parser.add_argument('-z', '--hosted-zone-id', metavar='hosted_zone_id', required=True, help="AWS hosted zone id")
 args = parser.parse_args()
 
-record_name = args.record
+record_names = args.record
 hosted_zone_id = args.hosted_zone_id
 
 r = requests.get('https://checkip.amazonaws.com')
@@ -27,9 +27,16 @@ if r.status_code != 200:
 public_ip = r.text.strip('\n')
 
 change_batch = {
-    'Comment': f'{record_name} DDNS record update',
+    'Comment': 'AWS DDNS record update',
     'Changes': [
-        {
+    ]
+}
+
+for record_name in record_names:
+    aws_record_ip = get_aws_record_ip(hosted_zone_id, record_name)
+
+    if aws_record_ip != public_ip:
+        record_batch = {
             'Action': 'UPSERT',
             'ResourceRecordSet': {
                 'Name': record_name,
@@ -42,16 +49,15 @@ change_batch = {
                 ]
             }
         }
-    ]
-}
+        change_batch['Changes'].append(record_batch)
+        print(f"{repr(record_name)} record IP ({aws_record_ip}) and public IP ({public_ip}) do not match, updating...")
 
-aws_record_ip = get_aws_record_ip(hosted_zone_id, record_name)
-if aws_record_ip != public_ip:
-    print(f"{repr(record_name)} record IP ({aws_record_ip}) and public IP ({public_ip}) do not match, updating...")
+if len(change_batch['Changes']) > 0:
     boto3.client('route53').change_resource_record_sets(HostedZoneId=hosted_zone_id, ChangeBatch=change_batch)
 
-    aws_record_ip = get_aws_record_ip(hosted_zone_id, record_name)
-    if aws_record_ip == public_ip:
-        print(f"{repr(record_name)} successfully updated to {aws_record_ip}")
-    else:
-        raise SystemExit("Record update failed")
+    for record_name in record_names:
+        aws_record_ip = get_aws_record_ip(hosted_zone_id, record_name)
+        if aws_record_ip == public_ip:
+            print(f"{repr(record_name)} successfully updated to {aws_record_ip}")
+        else:
+            raise SystemExit("Record update failed")
